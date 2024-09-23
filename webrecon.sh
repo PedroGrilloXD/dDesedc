68ba1571f18bcda8ed70557d9c99ffd1c5426bbb
Pedro Trugillo Garcia <155574001+PedroGrilloXD@users.noreply.github.com>
2024-09-23 07:11:18 -0300

    Create webrecon.sh

---
+++ webrecon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+while true; do 
+  echo "digite seu o dominio desejado:"
+read dominio
+  echo "digite a world list desejada:"
+read world
+  for palavra in $(cat $world);
+do 
+resposta=$(curl -s -o /dev/null -w "%{http_encode}" $dominio/$palavra/)
+  if [$resposta == 200]
+then
+  echo "diretorio encontrado: $palavra"
+fi 
+  done
+
+  echo "deseja realizar outra pesquisa? (s/n)"
+  read continuar 
+
+  if [[ ! "continuar" =~ ^[sS]$ ]]; then
+  break 
+fi
+done
